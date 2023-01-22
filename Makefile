3ef4e3f37c014f9e6880142e287343d43b56b719
Peter Zheng <peterzheng@Peters-Air-2.home>
2023-01-22 12:08:05 -0800

    First commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+game:
+	
+
+#clang++ main.cpp -o test -I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2
+
+
+	clang++ main.cpp -o Dock -I/opt/homebrew/include/SDL2 -L/opt/homebrew/lib -lSDL2 
---
+++ main.cpp
@@ -0,0 +1,56 @@
+// Example program:
+// Using SDL2 to create an application window
+
+#include <SDL.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "Dock Helper",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        1000,                               // width, in pixels
+        1000,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN){
+                quit = true;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+    }
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+    return 0;
+}
